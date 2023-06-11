ccd1604fdac6de8c99ae1c99f1234181b9030b8e
Hamza Aftab <bhattihamza0099@gmail.com>
2023-06-11 13:30:02 +0500

    refactor move interfaces to entities folder

--- src/hooks/useGame.tsx
+++ src/hooks/useGame.tsx
@@ -1,8 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
-import React from "react";
 import ApiClient from "../services/api-client";
-import { Game } from "./useGames";
+import { Game } from "../entities/Game";
 const apiClient = new ApiClient<Game>("/games");
 const useGame = (slug: string) =>
   useQuery({
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,12 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import ApiClient from "../services/api-client";
 import genres from "../data/genres";
 import ms from "ms";
+import { Genre } from "../entities/Genre";
 const apiClient = new ApiClient<Genre>("/genres");
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
 const useGenre = () =>
   useQuery({
     queryKey: ["genres"],
