e902f1b6a23e7ed6b8b54173c5bba2d0d2033882
Hamza Aftab <bhattihamza0099@gmail.com>
2023-06-12 15:46:13 +0500

    improve layout

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import ItemDefinition from "../components/ItemDefinition";
 import CriticScore from "../components/CriticScore";
@@ -14,13 +14,17 @@ const GameDetailPage = () => {
   if (isLoading) return <Spinner></Spinner>;
   if (error) throw error;
   return (
-    <>
-      <Heading>{game?.name}</Heading>
-      <ExpandableText>{String(game?.description_raw)}</ExpandableText>
-      {game && <GameAttributes game={game} />}
-      {game && <GameTrailors gameId={game?.id}></GameTrailors>}
-      {game && <GameScreenshots gameId={game?.id}></GameScreenshots>}
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <Box>
+        <Heading>{game?.name}</Heading>
+        <ExpandableText>{String(game?.description_raw)}</ExpandableText>
+        {game && <GameAttributes game={game} />}
+      </Box>
+      <Box>
+        {game && <GameTrailors gameId={game?.id}></GameTrailors>}
+        {game && <GameScreenshots gameId={game?.id}></GameScreenshots>}
+      </Box>
+    </SimpleGrid>
   );
 };
 
